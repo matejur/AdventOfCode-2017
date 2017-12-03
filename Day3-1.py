25e6b4edd06fbef5c88f0486d52de6e724a87bec
Matej Urbas <matej.urbas00@gmail.com>
2017-12-03 19:53:04 +0100

    English language friendly changes

--- Day3-1.py
+++ Day3-1.py
@@ -1,28 +1,28 @@
 import time
 
 input_number = 265149
-def move_right(x,y,razmik):
-    return(x+razmik,y)
+def move_right(x,y,dif):
+    return(x+dif,y)
 
-def move_left(x,y,razmik):
-    return(x-razmik,y)
+def move_left(x,y,dif):
+    return(x-dif,y)
 
-def move_up(x,y,razmik):
-    return(x,y+razmik)
+def move_up(x,y,dif):
+    return(x,y+dif)
 
-def move_down(x,y,razmik):
-    return(x,y-razmik)
+def move_down(x,y,dif):
+    return(x,y-dif)
 
-def xrazmik(x,y, razmik):
-    return(x+razmik,y)
+def xrazmik(x,y, dif):
+    return(x+dif,y)
 
-def yrazmik(x,y, razmik):
-    return(x,y+razmik)
+def yrazmik(x,y, dif):
+    return(x,y+dif)
 
 start_time = time.time()
 row_length = 0
 steps = 0
-razmik = 0
+dif = 0
 counter = 0
 
 x = 1
@@ -34,39 +34,39 @@ while x<input_number:
         row_length += 1
     if x+row_length > input_number:
             while x < input_number:
-                razmik+=1
+                dif+=1
                 x+=1
             break
 
     x += row_length
     steps += 1
 
-step_razmik = 1    
+step_dif = 1    
 for step in range(steps):
     if step%4 == 0:
-        coord = move_right(coord[0],coord[1],step_razmik)
+        coord = move_right(coord[0],coord[1],step_dif)
         #print("right", coord)
     if step%4 == 1:
-        coord = move_up(coord[0],coord[1],step_razmik)
+        coord = move_up(coord[0],coord[1],step_dif)
         #print("up", coord)
     if step%4 == 2:
-        coord = move_left(coord[0],coord[1],step_razmik)
+        coord = move_left(coord[0],coord[1],step_dif)
         #print("left", coord)
     if step%4 == 3:
-        coord = move_down(coord[0],coord[1],step_razmik)
+        coord = move_down(coord[0],coord[1],step_dif)
         #print("down", coord)
     if step%2 != 0 and step != 0:
-        step_razmik += 1
+        step_dif += 1
 
 if range(steps+1)[-1]%4 == 0:
-    coord = xrazmik(coord[0],coord[1], razmik)
+    coord = xrazmik(coord[0],coord[1], dif)
 if range(steps+1)[-1]%4 == 1:
-    coord = yrazmik(coord[0],coord[1], razmik)
+    coord = yrazmik(coord[0],coord[1], dif)
 if range(steps+1)[-1]%4 == 2:
-    coord = xrazmik(coord[0],coord[1], -razmik)
+    coord = xrazmik(coord[0],coord[1], -dif)
 if range(steps+1)[-1]%4 == 3:
-    coord = yrazmik(coord[0],coord[1], -razmik)
+    coord = yrazmik(coord[0],coord[1], -dif)
     
 
 print(abs(coord[0])+abs(coord[1]), time.time()-start_time)
-#print("Korak: {}, Razmik: {}, Coord: {}". format(steps, razmik, coord))    
+#print("Korak: {}, Razmik: {}, Coord: {}". format(steps, dif, coord))    
