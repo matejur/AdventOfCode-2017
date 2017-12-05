548ebfafa37f08011951112000682dd8865ba0d9
Matej Urbas <matej.urbas00@gmail.com>
2017-12-05 14:05:02 +0100

    Add files via upload

---
+++ Day5-2.py
@@ -0,0 +1,19 @@
+index = 0
+counter = 0
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    lines = list(map(int, lines))
+
+    while True:
+        next_move = lines[index]
+        if next_move >= 3:
+            lines[index] -= 1
+        else:
+            lines[index] += 1
+        index += next_move
+        counter += 1
+        
+        if index > len(lines)-1:
+            print(counter)
+            break
